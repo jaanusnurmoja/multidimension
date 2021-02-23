65f7747f2a9a17b8c08ca5bcd818be253b7e2c88
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2021-02-23 18:56:23 +0200

    Possible improvements

--- src/main/java/ee/net/nurmoja/multidimension/service/Helper.java
+++ src/main/java/ee/net/nurmoja/multidimension/service/Helper.java
@@ -1,24 +1,24 @@
 package ee.net.nurmoja.multidimension.service;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class Helper {
-    public static Long multiply(Iterable<Long> numbers) {
-        Long total = 1L;
-        for (Long x : numbers)
+    public static <E> Number multiply(Collection<E> numbers) {
+        Integer total = 1;
+        for (E x : numbers)
         {
-            total *= x;
+            total *= (Integer) x;
         }
         return total;
     }
-    public static Integer multiply(Integer[] numbers) {
+
+    public static <T> Integer multiply(T[] numbers) {
         int total = 1;
-        for (int x : numbers)
+        for (T x : numbers)
         {
-            total *= x;
+            total *= (int) x;
         }
         return total;
     }
+
 }
