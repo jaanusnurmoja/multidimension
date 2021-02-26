3ab166556dc113710980595335885e2eb6c5ed45
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2021-02-26 18:41:56 +0200

    Editing via form works, except reordering

--- src/main/java/ee/net/nurmoja/multidimension/controller/BlogFormController.java
+++ src/main/java/ee/net/nurmoja/multidimension/controller/BlogFormController.java
@@ -7,12 +7,7 @@ import ee.net.nurmoja.multidimension.repository.BlogPostParagraphRepository;
 import ee.net.nurmoja.multidimension.repository.BlogPostRepository;
 import ee.net.nurmoja.multidimension.repository.BlogPostSubPartRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,11 +20,10 @@ public class BlogFormController {
     final private BlogPostParagraphRepository paragraphRepository;
 
     @PostMapping("/data/api/blogPosts/add")
-    String create(@RequestBody(required = false) BlogPost blogPost){
+    String create(@RequestBody(required = false) BlogPost blogPost) {
         BlogPost savedBlogPost = blogPostRepository.save(blogPost);
 
-        if (blogPost.getBlogPostSubParts() != null)
-        {
+        if (blogPost.getBlogPostSubParts() != null) {
             int x = 0;
             for (BlogPostSubPart subPart : blogPost.getBlogPostSubParts()) {
                 subPart.setBlogPost(savedBlogPost);
@@ -45,7 +39,8 @@ public class BlogFormController {
                         paragraph.setOrdering(i);
                         i++;
                         paragraphRepository.save(paragraph);
-                    };
+                    }
+                    ;
                 }
                 x++;
             }
@@ -60,36 +55,32 @@ public class BlogFormController {
             RequestMethod.PUT,
             RequestMethod.PATCH
     })
-    String edit(@RequestBody(required = false) BlogPost blogPost, @PathVariable("id") Long id){
-        if (!blogPost.getBlogPostSubParts().isEmpty()) {
+    String edit(@RequestBody(required = false) BlogPost blogPost, @PathVariable("id") Long id) {
+        if (blogPost.getBlogPostSubParts().size() > 0) {
             int i = 0;
             for (BlogPostSubPart subPart : blogPost.getBlogPostSubParts()) {
-                if (subPart != null) {
-                    if (subPart.getBlogPost() != blogPost) subPart.setBlogPost(blogPost);
-                    subPart.setPrivateSysTitle("from: " + blogPost.getTitle().trim());
+                if (subPart.getBlogPost() != blogPost) subPart.setBlogPost(blogPost);
+                subPart.setPrivateSysTitle("from: " + blogPost.getTitle().trim());
 
-                    subPart.setOrdering(i);
-                   BlogPostSubPart saved =  subPartRepository.save(subPart);
-                    if (subPart.getBlogPostParagraphs().size() > 0) {
-                        int x = 0;
-                        for (BlogPostParagraph paragraph : subPart.getBlogPostParagraphs()) {
-                            if (paragraph != null) {
-                                if (paragraph.getBlogPostSubPart() != saved) paragraph.setBlogPostSubPart(subPart);
-                                if (!paragraph.getBlogPostId().equals(blogPost.getId())) paragraph.setBlogPostId(blogPost.getId());
-                                paragraph.setOrdering(x);
-                                paragraphRepository.save(paragraph);
-                                x++;
-                            }
-                        }
+                subPart.setOrdering(i);
+                BlogPostSubPart saved = subPartRepository.save(subPart);
+                if (subPart.getBlogPostParagraphs().size() > 0) {
+                    int x = 0;
+                    for (BlogPostParagraph paragraph : subPart.getBlogPostParagraphs()) {
+                        if (paragraph.getBlogPostSubPart() != saved) paragraph.setBlogPostSubPart(subPart);
+                        if (!paragraph.getBlogPostId().equals(blogPost.getId()))
+                            paragraph.setBlogPostId(blogPost.getId());
+                        paragraph.setOrdering(x);
+                        paragraphRepository.save(paragraph);
+                        x++;
                     }
-                    i++;
                 }
+                i++;
             }
         }
         BlogPost editedBlogPost = blogPostRepository.save(blogPost);
         return editedBlogPost.getId().toString();
     }
-
 
 
 }
